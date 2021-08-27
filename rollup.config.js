c131a8a269a38ab637d43482a18b0e0f9cced2b4
Ivan Georgiev <ivan_georgiew@yahoo.com>
2021-08-27 17:37:59 +0300

    removed browser field

--- rollup.config.js
+++ rollup.config.js
@@ -5,17 +5,17 @@ import pkg from './package.json'
 
 const entries = [
     ['.', 'TiedUp'],
-    ['./server', 'TiedUpServer']
+    ['./server', 'TiedUpServer'],
 ]
 const globals = {
     'tied-up': 'TiedUp',
-    'tied-up/server': 'TiedUpServer'
+    'tied-up/server': 'TiedUpServer',
 }
 const commonOutOpts = {
     esModule: false,
     freeze: false,
     exports: 'named',
-    sourcemap: true
+    sourcemap: true,
 }
 const makeInput = root => `${root}/src/index.js`
 const external = id => id.startsWith('tied-up')
@@ -24,17 +24,17 @@ const terserOpts = {
     format: {
         preserve_annotations: true,
         wrap_iife: true,
-        wrap_func_args: true
+        wrap_func_args: true,
     },
     compress: {
         keep_infinity: true,
         pure_getters: true,
-        passes: 3
-    }
+        passes: 3,
+    },
 }
 const treeshake = {
     propertyReadSideEffects: false,
-    tryCatchDeoptimization: false
+    tryCatchDeoptimization: false,
 }
 const reducer = (acc, [root, name]) =>
     acc.concat([
@@ -43,13 +43,19 @@ const reducer = (acc, [root, name]) =>
             external,
             treeshake,
             output: [
-                { ...commonOutOpts, format: 'cjs', file: `${root}/${pkg.main}` },
+                {
+                    ...commonOutOpts,
+                    format: 'cjs',
+                    file: `${root}/${pkg.main}`,
+                    plugins: [terser(terserOpts)],
+                },
                 {
                     ...commonOutOpts,
                     format: 'esm',
-                    file: `${root}/${pkg.module}`
-                }
-            ]
+                    file: `${root}/${pkg.module}`,
+                    plugins: [terser({ ...terserOpts, module: true })],
+                },
+            ],
         },
         {
             input: makeInput(root),
@@ -58,27 +64,21 @@ const reducer = (acc, [root, name]) =>
             output: [
                 {
                     ...commonOutOpts,
-                    format: 'esm',
-                    file: `${root}/${pkg.browser}`,
-                    plugins: [terser({ ...terserOpts, module: true })]
-                },
-                {
-                    ...commonOutOpts,
                     format: 'umd',
                     file: `${root}/${pkg.unpkg}`,
                     plugins: [terser(terserOpts)],
                     name,
-                    globals
-                }
+                    globals,
+                },
             ],
             plugins: [
                 replace({
                     'process.env.NODE_ENV': JSON.stringify('production'),
-                    preventAssignment: true
+                    'preventAssignment': true,
                 }),
-                strip({ functions: ['or'] })
-            ]
-        }
+                strip({ functions: ['or'] }),
+            ],
+        },
     ])
 
 export default entries.reduce(reducer, [])
