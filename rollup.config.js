e269c4f1a706a900a50ca0ccfa8789b60013a597
Ivan Georgiev <ivan_georgiew@yahoo.com>
2021-08-28 16:52:33 +0300

    build changes

--- rollup.config.js
+++ rollup.config.js
@@ -7,18 +7,22 @@ const entries = [
     ['.', 'TiedUp'],
     ['./server', 'TiedUpServer'],
 ]
+
 const globals = {
     'tied-up': 'TiedUp',
     'tied-up/server': 'TiedUpServer',
 }
+
 const commonOutOpts = {
     esModule: false,
     freeze: false,
     exports: 'named',
-    sourcemap: true,
 }
+
 const makeInput = root => `${root}/src/index.js`
+
 const external = id => id.startsWith('tied-up')
+
 const terserOpts = {
     ecma: 6,
     format: {
@@ -32,10 +36,12 @@ const terserOpts = {
         passes: 3,
     },
 }
+
 const treeshake = {
     propertyReadSideEffects: false,
     tryCatchDeoptimization: false,
 }
+
 const reducer = (acc, [root, name]) =>
     acc.concat([
         {
@@ -43,18 +49,8 @@ const reducer = (acc, [root, name]) =>
             external,
             treeshake,
             output: [
-                {
-                    ...commonOutOpts,
-                    format: 'cjs',
-                    file: `${root}/${pkg.main}`,
-                    plugins: [terser(terserOpts)],
-                },
-                {
-                    ...commonOutOpts,
-                    format: 'esm',
-                    file: `${root}/${pkg.module}`,
-                    plugins: [terser({ ...terserOpts, module: true })],
-                },
+                { ...commonOutOpts, format: 'cjs', file: `${root}/${pkg.main}` },
+                { ...commonOutOpts, format: 'esm', file: `${root}/${pkg.module}` },
             ],
         },
         {
@@ -64,17 +60,30 @@ const reducer = (acc, [root, name]) =>
             output: [
                 {
                     ...commonOutOpts,
+                    sourcemap: true,
+                    format: 'cjs',
+                    file: `${root}/${pkg.mainProd}`,
+                },
+                {
+                    ...commonOutOpts,
+                    sourcemap: true,
+                    format: 'esm',
+                    file: `${root}/${pkg.moduleProd}`,
+                },
+                {
+                    ...commonOutOpts,
+                    sourcemap: true,
                     format: 'umd',
                     file: `${root}/${pkg.unpkg}`,
-                    plugins: [terser(terserOpts)],
                     name,
                     globals,
                 },
             ],
             plugins: [
+                terser(terserOpts),
                 replace({
-                    'process.env.NODE_ENV': JSON.stringify('production'),
                     'preventAssignment': true,
+                    'process.env.NODE_ENV': JSON.stringify('production'),
                 }),
                 strip({ functions: ['or'] }),
             ],
